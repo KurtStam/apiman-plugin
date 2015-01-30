674be60bfd282d958f6ae6029017ef6ea98c94c7
Kurt Stam <kstam@redhat.com>
2015-01-29 22:06:17 -0500

    deleting generated files

--- d.ts/apiman/ts/apimanPlugin.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../includes.d.ts" />
-/// <reference path="apimanGlobals.d.ts" />
-/// <reference path="services.d.ts" />
-declare module Apiman {
-    var _module: ng.IModule;
-}
--- defs.d.ts
+++ defs.d.ts
@@ -1,3 +1,6 @@
+/// <reference path="d.ts/includes.d.ts"/>
+/// <reference path="d.ts/apiman/ts/apimanGlobals.d.ts"/>
+/// <reference path="d.ts/apiman/ts/services.d.ts"/>
 /// <reference path="d.ts/apiman/ts/apimanPlugin.d.ts"/>
 /// <reference path="d.ts/apiman/ts/consumer-orgs.d.ts"/>
 /// <reference path="d.ts/apiman/ts/dash.d.ts"/>
